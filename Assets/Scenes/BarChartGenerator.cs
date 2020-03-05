1c45d283e949b1d8a806f4ecde9c6ec622aa590e
Michael Kintscher <michael.troop90@gmail.com>
2020-03-04 17:32:43 -0700

    Bar Chart ticks added. Previously unkown bugs fixed. Code simplified.

--- Assets/Scenes/BarChartGenerator.cs
+++ Assets/Scenes/BarChartGenerator.cs
@@ -12,6 +12,20 @@ public class BarChartGenerator : MonoBehaviour
     // The size of the chart, configured in the Unity editor.
     public Vector3 ChartSize;
 
+    // The size of the spacing between bars in the chart. Zero
+    //      corresponds to no spacing. The absolute value is taken,
+    //      so negative values are equivalent to their positive
+    //      counterparts.
+    public float GutterSize;
+
+    // Controls whether the ticks appear on the bars.
+    public bool DisplayTicksOnBars;
+
+    // Controls the interval of the ticks.
+    public float TickFrequency;
+
+    private float BarTickOffset = 0.005f;
+
     // Stores the bars in the chart.
     private List<GameObject> Bars { get; set; }
 
@@ -26,14 +40,7 @@ public class BarChartGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (this.TestMode)
-        {
-            this.InitializeBarsTest();
-        }
-        else
-        {
-            this.InitializeBars();
-        }
+        this.InitializeBars();
         this.InitializeAxes();
     }
 
@@ -41,77 +48,6 @@ public class BarChartGenerator : MonoBehaviour
     void Update()
     {
         
-    }
-
-    private void InitializeBarsTest()
-    {
-        this.Bars = new List<GameObject>();
-
-        float tallestBarHeight = 0.0f;
-
-        // Check to see that the chart can be meaningfully be created.
-        //      Returning prevents any divide by zero errors in sizing.
-        if (this.ChartSize.x == 0 || this.ChartSize.y == 0 || this.ChartSize.z == 0)
-        {
-            Debug.LogWarning("Bar Chart cannot be drawn. One of the Chart size dimensions is set to zero.");
-            return;
-        }
-        if (this.NumOfColumns == 0 || this.NumOfRows == 0)
-        {
-            Debug.LogWarning("Bar Chart cannot be drawn. The number of columns or number of rows is set to zero for the test data.");
-            return;
-        }
-
-        // Calculate the size of each bar. Bars are evenly distributed across the
-        //      total given space, so the size of an individual bar is simply the
-        //      total divided by the number of bars.
-        this.BarSize = new Vector2(
-            this.ChartSize.x / this.NumOfRows,
-            this.ChartSize.z / this.NumOfColumns
-            );
-
-        // Generate the bars.
-        for (int x = 0; x < this.NumOfRows; x++)
-        {
-            for (int z = 0; z < this.NumOfColumns; z++)
-            {
-                // Create a new cube to model a bar in the bar chart.
-                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-
-                // Set the height to a random number.
-                float height = Random.value * this.ChartSize.z;
-                if (height > tallestBarHeight)
-                {
-                    tallestBarHeight = height;
-                }
-
-                // Scale the bar based on the the previously calculated bar size.
-                cube.transform.localScale = new Vector3(this.BarSize.x, height, this.BarSize.y);
-
-                // Position the bar so that it is adjacent to the previous bar.
-                //      Note that the origin is in the middle of the first bar by default,
-                //      so each bar must be shifted by half of the bar size.
-                cube.transform.position = new Vector3(
-                    (this.BarSize.x / 2.0f) + x * this.BarSize.x,
-                    0,
-                    (this.BarSize.y / 2.0f) + z * this.BarSize.y);
-
-                // Add the bar to the list.
-                this.Bars.Add(cube);
-            }
-        }
-
-        // Adjust the vertical positioning of the bars so that they are all bottom-aligned.
-        foreach (GameObject cube in this.Bars)
-        {
-            // Note that a vertical offset is necessary to ensure the bottoms of the cubes
-            //      are aligned, rather than the centers.
-            Vector3 oldPosition = cube.transform.position;
-            cube.transform.position = new Vector3(
-                oldPosition.x,
-                (cube.transform.localScale.y) / 2.0f,
-                oldPosition.z);
-        }
     }
 
     private void InitializeBars()
@@ -127,50 +63,86 @@ public class BarChartGenerator : MonoBehaviour
             Debug.LogWarning("Bar Chart cannot be drawn. One of the Chart size dimensions is set to zero.");
             return;
         }
-
-        // Get a reference to the file data reader.
-        this.FileDataReader = this.GetComponent(typeof(CsvDataReader3D)) as CsvDataReader3D;
-
-        // If no file data reader component is found, revert back to the test
-        //      chart data, and print a warning to the Unity console.
-        if (this.FileDataReader == null)
+        if (this.DisplayTicksOnBars && this.TickFrequency <= 0)
         {
-            this.InitializeBarsTest();
-            Debug.LogWarning("No CsvDataReader3D component found. Reverting back to test chart data.");
+            Debug.LogWarning("Ticks cannot be drawn. The tick frequency must be greater than zero.");
+        }
+        if (this.TestMode && this.NumOfColumns == 0 || this.NumOfRows == 0)
+        {
+            Debug.LogWarning("Bar Chart cannot be drawn. The number of columns or number of rows is set to zero for the test data.");
             return;
         }
 
-        // Get the data from the file data reader.
-        List<Vector3> data = this.FileDataReader.GetData();
+        List<Vector3> data = new List<Vector3>();
 
-        // Measure the number of bars needed in each dimension.
-        //      This code currently assumes all values are positive.
-        this.NumOfRows = 0;
-        this.NumOfColumns = 0;
-        foreach (Vector3 barData in data)
+        if (this.TestMode)
         {
-            if (barData.x > this.NumOfRows)
+            // Generate the data for the bars.
+            for (int row = 0; row < this.NumOfRows; row++)
             {
-                this.NumOfRows = Mathf.RoundToInt(barData.x);
+                for (int column = 0; column < this.NumOfColumns; column++)
+                {
+                    // Set the height to a random number.
+                    data.Add(new Vector3(
+                        row,
+                        column,
+                        Random.value * this.ChartSize.y
+                        ));
+                }
             }
-            if (barData.y > this.NumOfColumns)
+
+            // This will cancel out the operation setting the bar's Y scale, so that
+            //      the scale is just the bar's Z data.
+            tallestBarHeight = this.ChartSize.y;
+        }
+        else
+        {
+            // Get a reference to the file data reader.
+            this.FileDataReader = this.GetComponent(typeof(CsvDataReader3D)) as CsvDataReader3D;
+
+            // If no file data reader component is found, revert back to the test
+            //      chart data, and print a warning to the Unity console.
+            if (this.FileDataReader == null)
             {
-                this.NumOfColumns = Mathf.RoundToInt(barData.y);
+                this.InitializeBars();
+                Debug.LogWarning("No CsvDataReader3D component found. Reverting back to test chart data.");
+                return;
             }
-            if (barData.z > tallestBarHeight)
+
+            // Get the data from the file data reader.
+            data = this.FileDataReader.GetData();
+
+            // Measure the number of bars needed in each dimension.
+            //      This code currently assumes all values are positive.
+            this.NumOfRows = 0;
+            this.NumOfColumns = 0;
+            foreach (Vector3 barData in data)
             {
-                tallestBarHeight = barData.z;
+                if (barData.x > this.NumOfRows)
+                {
+                    this.NumOfRows = Mathf.RoundToInt(barData.x);
+                }
+                if (barData.y > this.NumOfColumns)
+                {
+                    this.NumOfColumns = Mathf.RoundToInt(barData.y);
+                }
+                if (barData.z > tallestBarHeight)
+                {
+                    tallestBarHeight = barData.z;
+                }
             }
+
+            // Accounts for zero-indexed file.
+            this.NumOfRows++;
+            this.NumOfColumns++;
         }
 
         // Calculate the size of each bar. Bars are evenly distributed across the
         //      total given space, so the size of an individual bar is simply the
         //      total divided by the number of bars.
-        //      Note that one must be added to the row and column numbers, since
-        //          the input data is zero indexed.
         this.BarSize = new Vector2(
-            this.ChartSize.x / (this.NumOfRows + 1),
-            this.ChartSize.z / (this.NumOfColumns + 1)
+            this.ChartSize.x / this.NumOfRows,
+            this.ChartSize.z / this.NumOfColumns
             );
 
         // Generate the bars.
@@ -179,21 +151,89 @@ public class BarChartGenerator : MonoBehaviour
             // Create a new cube to model a bar in the bar chart.
             GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
 
+            // Make the bar a child of the chart.
+            cube.transform.parent = this.gameObject.transform;
+
+            float height = barData.z;
+
+            // Display the tick marks on the bar.
+            if (this.DisplayTicksOnBars)
+            {
+                // The bar needs a tick for each tick frequency, plus one for the 
+                //      bottom of the bar. Take the floor of this number, because
+                //      whatever is left at the top will not have a tick above it.
+                int numOfTicks = Mathf.FloorToInt(height / this.TickFrequency) + 1;
+
+                // For each tick on the bar...
+                for (int i = 0; i < numOfTicks; i++)
+                {
+                    // Create a game object to hold the tick.
+                    GameObject tickObject = new GameObject();
+
+                    // Parent the bar to the tick game object.
+                    tickObject.transform.parent = cube.transform;
+
+                    // Create the line renderer components on the tick object, 
+                    //      one for each side of the bar.
+                    LineRenderer tickRenderer = tickObject.AddComponent(typeof(LineRenderer)) as LineRenderer;
+                    tickRenderer.useWorldSpace = false;
+
+                    // Define the tick endpoint positions for each side.
+                    //      The Y starts at negative half of the scale, and goes up in
+                    //          increments based on the tick frequency. The division by
+                    //          height is to account for the scaling later (code is in
+                    //          wrong order... should have been scaled first).
+                    //      The X and Z are going to be offset by half of the cube size
+                    //          in their respective directions, depending on which side
+                    //          of the bar.
+                    Vector3[] leftPositions = new Vector3[5]
+                    {
+                            new Vector3(-cube.transform.localScale.x / 2 - this.BarTickOffset,
+                                        -cube.transform.localScale.y / 2 + (this.TickFrequency /height) * i,
+                                        -cube.transform.localScale.z / 2 - this.BarTickOffset),
+                            new Vector3(-cube.transform.localScale.x / 2 - this.BarTickOffset,
+                                        -cube.transform.localScale.y / 2 + (this.TickFrequency /height) * i,
+                                        cube.transform.localScale.z / 2 + this.BarTickOffset),
+                            new Vector3(cube.transform.localScale.x / 2 + this.BarTickOffset,
+                                        -cube.transform.localScale.y / 2 + (this.TickFrequency /height) * i,
+                                        cube.transform.localScale.z / 2 + this.BarTickOffset),
+                            new Vector3(cube.transform.localScale.x / 2 + this.BarTickOffset,
+                                        -cube.transform.localScale.y / 2 + (this.TickFrequency /height) * i,
+                                        -cube.transform.localScale.z / 2 - this.BarTickOffset),
+                            new Vector3(-cube.transform.localScale.y / 2 - this.BarTickOffset,
+                                        -cube.transform.localScale.x / 2 + (this.TickFrequency /height) * i,
+                                        -cube.transform.localScale.z / 2 - this.BarTickOffset),
+                    };
+
+                    // Set the tick endpoint positions.
+                    tickRenderer.positionCount = leftPositions.Length;
+                    tickRenderer.SetPositions(leftPositions);
+
+                    // Set the start and end colors for each side.
+                    tickRenderer.startColor = Color.red;
+                    tickRenderer.endColor = Color.red;
+
+                    // Set the start and end widths for each side.
+                    tickRenderer.startWidth = 0.01f;
+                    tickRenderer.endWidth = 0.01f;
+                }
+            }
+
             // Scale the bar based on the the previously calculated bar size.
-            //      Note that the "Z" value of the data is placed in the "Y"
-            //      scale. This is because Unity has the Y axis pointing up
-            //      instead of the Z axis.
-            cube.transform.localScale = new Vector3(this.BarSize.x, barData.z, this.BarSize.y);
+            //      The height is scaled such that the tallest bar vertically
+            //          fills the chart.
+            cube.transform.localScale = new Vector3(
+                this.BarSize.x - Mathf.Abs(this.GutterSize),
+                this.ChartSize.y * (height / tallestBarHeight),
+                this.BarSize.y - Mathf.Abs(this.GutterSize));
 
             // Position the bar so that it is adjacent to the previous bar.
             //      Note that the origin is in the middle of the first bar by default,
-            //          so each bar must be shifted by half of the bar size.
-            //      Also note: Adjust the vertical positioning of the bars so that they
-            //          are all bottom-aligned by moving them up by 1/2 their Y scale.
-            cube.transform.position = new Vector3(
-                (this.BarSize.x / 2.0f) + Mathf.RoundToInt(barData.x) * this.BarSize.x,
-                (cube.transform.localScale.y) / 2.0f,
-                (this.BarSize.y / 2.0f) + Mathf.RoundToInt(barData.y) * this.BarSize.y);
+            //      so each bar must be shifted by half of the bar size.
+            cube.transform.localPosition = new Vector3(
+                (this.BarSize.x / 2.0f) + barData.x * this.BarSize.x,
+                cube.transform.localScale.y / 2f,
+                (this.BarSize.y / 2.0f) + barData.y * this.BarSize.y);
 
             // Add the bar to the list.
             this.Bars.Add(cube);
@@ -234,5 +274,21 @@ public class BarChartGenerator : MonoBehaviour
         this.XAxisRenderer.SetPositions(xAxispositions);
         this.YAxisRenderer.SetPositions(yAxispositions);
         this.ZAxisRenderer.SetPositions(zAxispositions);
+
+        // Set the start and end colors for each axis.
+        this.XAxisRenderer.startColor = Color.red;
+        this.XAxisRenderer.endColor = Color.red;
+        this.YAxisRenderer.startColor = Color.green;
+        this.YAxisRenderer.endColor = Color.green;
+        this.ZAxisRenderer.startColor = Color.blue;
+        this.ZAxisRenderer.endColor = Color.blue;
+
+        // Set the start and end widths for each axis.
+        this.XAxisRenderer.startWidth = 0.005f;
+        this.XAxisRenderer.endWidth = 0.005f;
+        this.YAxisRenderer.startWidth = 0.005f;
+        this.YAxisRenderer.endWidth = 0.005f;
+        this.ZAxisRenderer.startWidth = 0.005f;
+        this.ZAxisRenderer.endWidth = 0.005f;
     }
 }
